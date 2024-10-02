ad74c5558db8832eaaedbbec78bca3d381943779
Nabukenya Rehemah <remmiehayaan@gmail.com>
2024-10-02 10:26:38 +0300

    creating the app

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div>
+      <h2>App</h2>
+      <Header/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
